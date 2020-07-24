7359186b3e6375be22193ebf833a9102656cb105
longnphan <67768035+longnphan@users.noreply.github.com>
2020-07-24 15:56:06 -0600

    Add files via upload

--- App.js
+++ App.js
@@ -1,4 +1,3 @@
-import "./App.css";
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 
@@ -7,6 +6,16 @@ function App() {
   const [photo, setPhoto] = useState([]);
   const [date, setDate] = useState('');
   const [newDate, setNewDate] = useState('');
+
+//Today's date in YYYY-MM-DD format to set max date on date input
+let today = new Date();
+let dd = today.getDate();
+let mm = today.getMonth()+1;
+let yyyy = today.getFullYear();
+  if(dd<10)dd='0'+dd;
+  if(mm<10) mm='0'+mm;
+today = yyyy+'-'+mm+'-'+dd;
+//
 
   function handleChange(e) {
     setDate(e.target.value);
@@ -33,18 +42,17 @@ function App() {
   <div className="search-bar ui black inverted segment">
     <form onSubmit={handleChange} className="ui form">
       <div className="field">
-      <label style={{color: 'white'}}> Enter Date </label>
+      <label style={{color: 'white'}}> Select Date </label>
       <input
-        type="text"
-        value={date}
+        type="date"
+        min="1995-06-20"  //minimum date on picture of the day
+        max={today}
         onChange={handleChange}
-        placeholder="e.g. 2020-06-30"
-    />
-
+       />
     <button className="ui inverted button" onClick={handleClick}>Submit</button>
 
-        </div>
-        </form>
+      </div>
+    </form>
   </div>
 
   <div className="ui raised very black inverted segment" id="image-div">
